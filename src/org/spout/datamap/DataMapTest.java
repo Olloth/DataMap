6f13aabd3517c54948044bed5f41c52e89f41fc7
Dalton Pelc <daltonjpelc@gmail.com>
2012-09-19 14:45:47 -0400

    Reorganized the test objects into a DataMapTest
    Signed-off-by: Dalton Pelc <daltonjpelc@gmail.com>

--- src/org/spout/datamap/DataMapTest.java
+++ src/org/spout/datamap/DataMapTest.java
@@ -8,18 +8,22 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class TestDataSuperObject implements DataHolder {
+public class DataMapTest {
 
-	private DataMap dataMap = new DataMap();
+	private DataMap dataMap;
+
 	private TestDataSubObject testSubObject = new TestDataSubObject(-99, "test string here!");
 	private TestDataSubObject testSubObjectTwo = new TestDataSubObject(939, "test string here, again!");
 
 	private float[] arrayFloats = {-13.0f, 203.991f, -0.44f, 103435.3F};
 	private DataArray dataArray = new DataArray(DataType.FLOAT);
 
-	public TestDataSuperObject() {
+	@Before
+	public void setup() {
+		dataMap = new DataMap();
 		for (int i = 0; i < arrayFloats.length; i++ ) {
 			dataArray.add(new NumberData(arrayFloats[i]));
 		}
@@ -33,29 +37,27 @@ public class TestDataSuperObject implements DataHolder {
 		dataMap.put("testSubObject1", testSubObject.getData());
 		dataMap.put("testSubObject2", testSubObjectTwo.getData());
 		dataMap.put("testDataArray", dataArray);
-		
-	}
-
-	@Override
-	public DataMap getData() {
-		return dataMap;
 	}
 
 	@Test
-	public void testStringWrite() {
-		System.out.println(new TestDataSuperObject());
+	public void testPut() {
+		dataMap.put("testPut", new NumberData(39));
+		assertTrue(dataMap.containsKey("testPut"));
 	}
 
 	@Test
-	public void testBinary() {
-		TestDataSuperObject test = new TestDataSuperObject();
-		
-		DataMap testWrite = test.getData();
+	public void testGet() {
+		int testInt = (int) dataMap.get("testInt").getValue();
+		assertTrue(testInt == 1834854);
+	}
+
+	@Test
+	public void testReadWrite() {
 		ByteArrayOutputStream byteOS = new ByteArrayOutputStream();
 		DataOutputStream os = new DataOutputStream(byteOS);
 
 		try {
-			testWrite.write(os);
+			dataMap.write(os);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -65,9 +67,35 @@ public class TestDataSuperObject implements DataHolder {
 		
 		try {
 			DataMap testRead = DataMap.read(is);
-			assertTrue(testWrite.toString().equals(testRead.toString()));
+			assertTrue(dataMap.toString().equals(testRead.toString()));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+	
+	private class TestDataSubObject implements DataHolder {
+
+		private DataMap dataMap = new DataMap();
+
+		public TestDataSubObject(int intValue, String stringValue) {
+			int[] arrayInts = {1, 203, 44};
+			DataArray dataArray = new DataArray(DataType.INTEGER);
+			
+			for (int i = 0; i < arrayInts.length; i++ ) {
+				dataArray.add(new NumberData(arrayInts[i]));
+			}
+		
+			dataMap.put("subInt", new NumberData(intValue));
+			dataMap.put("subString", new StringData(stringValue));
+			dataMap.put("subArrayInts", dataArray);
+			
+		}
+
+		@Override
+		public DataMap getData() {
+			return dataMap;
+		}
+
+	}
+
 }
